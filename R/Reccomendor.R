fe36a0b345b35e316bf1d689677304e186c24d71
MELIEWELLS <MWELLS@SMTIH.EDU>
2024-04-16 21:14:08 -0400

    start on the song rec function

--- R/Reccomendor.R
+++ R/Reccomendor.R
@@ -6,3 +6,25 @@
 rec_song <- function(title) {
   return(title)
 }
+
+rec_song <- function(genre, mode, energy, loudness, valence, danceability, instrumentalness, min_duration, max_duration) {
+  # Filter dataframe based on genre and mode
+  filterd_song <- song_bank[song_bank$Genre == genre & song_bank$Mode == mode,]
+
+  # Filter dataframe based on energy, loudness, valence, danceability, instrumentalness, and duration
+  filterd_song <- filterd_song[filterd_song$Energy >= energy &
+                                 filterd_song$Loudness >= loudness &
+                                 filterd_song$Valence >= valence &
+                                 filterd_song$Danceability >= danceability &
+                                 filterd_song$Instrumentalness <= instrumentalness &
+                                 filterd_song$Duration >= min_duration &
+                                 filterd_song$Duration <= max_duration,]
+
+  # If multiple songs match the criteria, select one randomly
+  if (nrow(filterd_song) > 0) {
+    rec_song <- filterd_song[sample(nrow(filterd_song), 1),]
+    return(rec_song)
+  } else {
+    return(NULL)
+  }
+}
