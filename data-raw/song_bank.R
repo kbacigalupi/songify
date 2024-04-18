aff3461377b887087b6bd18069c9d166742e3dc5
MELIEWELLS <MWELLS@SMTIH.EDU>
2024-04-18 13:16:45 -0400

    trying to make my branch work

--- data-raw/song_bank.R
+++ data-raw/song_bank.R
@@ -1,44 +1,20 @@
 ## code to prepare `song_bank` dataset goes here
 
-usethis::use_data(song_bank, overwrite = TRUE)
 
 library(spotifyr)
-library(parallel)
 library(dplyr)
-library(purrr)
 
 secret <- '0bc3f31a7f674b7f969d69fb466d8822'
 Sys.setenv(SPOTIFY_CLIENT_ID = '0a9a4d1514b3400abb2f141a32dd0bac')
 Sys.setenv(SPOTIFY_CLIENT_SECRET = '29862f8cb29c41b18d9d234947009a19')
 
-genres <- c("pop", "pop rap", "dance pop", "house", "teen pop") #"electro house", "edm", "lounge", "classical", "soul", "r&b", "jazz")
 
-artists <- function(genre) {
-  Sys.sleep(0.5)
-  get_genre_artists(genre, limit = 50)
-}
+taylor <- get_genre_artists("pop", limit = 1)
 
-songdata <- function(artist) {
-  Sys.sleep(0.2)
-  get_artist_audio_features(artist)
-  print(artist)
-}
-
-artist_bank <- mclapply(genres, artists)
-
-#obviously bad code oops
-get_ids <- function(data) {
-  data$id
-}
-
-get_names <- function(data) {
-  data$name
-}
-
-artists_name <- unique(unlist(lapply(artist_bank, get_names)))
-artists_vector <- unique(unlist(lapply(artist_bank, get_ids)))
-
-song_bank <- lapply(artists_vector, songdata)
-  list_rbind() |>
+song_bank <- get_artist_audio_features(taylor$id) |>
   select("artist_name", "album_type", "danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "time_signature", "artists", "duration_ms", "explicit")
+
+song_bank <- song_bank[!is.na(song_bank)]
+
+usethis::use_data(song_bank, overwrite = TRUE)
 
