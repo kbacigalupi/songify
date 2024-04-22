4be0b514fd6110a44b6be050d9398a7c3c8a1ea4
Kathleen <kbacigalupi@smith.edu>
2024-04-22 08:55:45 -0400

    Removing API from the git version

--- data-raw/song_bank.R
+++ data-raw/song_bank.R
@@ -4,11 +4,7 @@
 library(spotifyr)
 library(dplyr)
 
-secret <- '0bc3f31a7f674b7f969d69fb466d8822'
-Sys.setenv(SPOTIFY_CLIENT_ID = '0a9a4d1514b3400abb2f141a32dd0bac')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = '29862f8cb29c41b18d9d234947009a19')
-
-
+source("API_raw.R")
 taylor <- get_genre_artists("pop", limit = 1)
 
 song_bank <- get_artist_audio_features(taylor$id) |>
