75c76d8228c7efe7ac6a7c446b1eaf5900d30033
MELIEWELLS <MWELLS@SMTIH.EDU>
2024-05-06 12:35:45 -0400

    skips test for r-cmd checks

--- tests/testthat/test-Reccomendor.R
+++ tests/testthat/test-Reccomendor.R
@@ -2,12 +2,15 @@
 
 # Tests that the function returns a character string
 test_that("rec_song returns a character string for valid input", {
+  skip_if(Sys.getenv("SPOTIFY_CLIENT_ID") == "" || Sys.getenv("SPOTIFY_CLIENT_SECRET") == "", "Spotify API credentials not available")
   result <- rec_song(genre = "rock", mode = 1, energy = 0.8, valence = 0.7)
   expect_type(result, "character")
 })
 
 # Tests if the user inputs an invalid genre
 test_that("rec_song handles invalid genre", {
+  skip_if(Sys.getenv("SPOTIFY_CLIENT_ID") == "" || Sys.getenv("SPOTIFY_CLIENT_SECRET") == "", "Spotify API credentials not available")
   result <- rec_song(genre = "invalid_genre")
   expect_equal(result, "No artists found for this genre, try again")
 })
+
