49ba801d122f3547a0c43387c43215e514c5c891
MELIEWELLS <MWELLS@SMTIH.EDU>
2024-05-07 13:35:03 -0400

    updates to rec_song tests

--- tests/testthat/test-Reccomendor.R
+++ tests/testthat/test-Reccomendor.R
@@ -1,10 +1,10 @@
 # credentials object is created in helper-credentials.R
 
-# Tests that the function returns a character string
-test_that("rec_song returns a character string for valid input", {
+# Tests that the function returns an object of the song class
+test_that("rec_song returns an object of the class song for valid input", {
   skip_if(Sys.getenv("SPOTIFY_CLIENT_ID") == "" || Sys.getenv("SPOTIFY_CLIENT_SECRET") == "", "Spotify API credentials not available")
   result <- rec_song(genre = "rock", mode = 1, energy = 0.8, valence = 0.7)
-  expect_type(result, "character")
+  expect_s3_class(result, "song")
 })
 
 # Tests if the user inputs an invalid genre
